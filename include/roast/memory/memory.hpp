1cb83b40369e3274bcd68d735a851f8272c63e13
myun2 <myun2@nwhite.info>
2013-03-13 12:37:19 +0900

    null Test!

--- include/roast/memory/memory.hpp
+++ include/roast/memory/memory.hpp
@@ -3,4 +3,6 @@
 #ifndef __SFJP_ROAST_roast__memory__memory_HPP__
 #define __SFJP_ROAST_roast__memory__memory_HPP__
 
+#include "roast/memory/null.hpp"
+
 #endif//__SFJP_ROAST_roast__memory__memory_HPP__
--- test.cpp
+++ test.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include "roast/memory.hpp"
 
 int main()
 {
+	null n();
 
 	return 0;
 }
