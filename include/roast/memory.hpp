331cc4b318577df266a20755bfd3f48119c353ba
myun2 <myun2@nwhite.info>
2013-03-13 11:54:06 +0900

    inner memory/memory.hpp Add

--- include/roast/memory.hpp
+++ include/roast/memory.hpp
@@ -3,4 +3,6 @@
 #ifndef __SFJP_ROAST_roast__memory_HPP__
 #define __SFJP_ROAST_roast__memory_HPP__
 
+#include "roast/memory/memory.hpp"
+
 #endif//__SFJP_ROAST_roast__memory_HPP__
---
+++ include/roast/memory/memory.hpp
@@ -0,0 +1,6 @@
+//	Roast+ License
+
+#ifndef __SFJP_ROAST_roast__memory__memory_HPP__
+#define __SFJP_ROAST_roast__memory__memory_HPP__
+
+#endif//__SFJP_ROAST_roast__memory__memory_HPP__
