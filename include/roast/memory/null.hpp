df70d04c5972b09e8673f8a72e09d7a4f3873c98
myun2 <myun2@nwhite.info>
2013-03-13 12:00:00 +0900

    memory/null.hpp Add

---
+++ include/roast/memory/null.hpp
@@ -0,0 +1,11 @@
+//	Roast+ License
+
+#ifndef __SFJP_ROAST_roast__memory__null_HPP__
+#define __SFJP_ROAST_roast__memory__null_HPP__
+
+class null
+{
+};
+
+typedef null null_type, NullType;
+#endif//__SFJP_ROAST_roast__memory__null_HPP__
